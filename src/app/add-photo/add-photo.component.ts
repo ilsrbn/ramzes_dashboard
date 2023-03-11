fe8f17f4d6c9f05ba9e4b66e914aefc4568ffe4d
illia.serbin <illia.serbin@tangramltd.com>
2023-03-11 19:41:07 +0200

    update files upload

--- src/app/add-photo/add-photo.component.ts
+++ src/app/add-photo/add-photo.component.ts
@@ -1,16 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import {NzUploadFile} from 'ng-zorro-antd/upload';
 import {Router} from '@angular/router';
-import {Observable} from 'rxjs';
+import {zip} from 'rxjs';
 import {AdminPhotoService} from '../api';
-
-const getBase64 = (file: File): Promise<string | ArrayBuffer | null> =>
-  new Promise((resolve, reject) => {
-    const reader = new FileReader();
-    reader.readAsDataURL(file);
-    reader.onload = () => resolve(reader.result);
-    reader.onerror = error => reject(error);
-  });
+import {tap, } from 'rxjs/operators';
 
 @Component({
   selector: 'app-add-photo',
@@ -26,16 +19,6 @@ export class AddPhotoComponent {
   }
 
   fileList: NzUploadFile[] = []
-  previewImage: string | undefined = '';
-  previewVisible = false;
-
-  handlePreview = async (file: NzUploadFile): Promise<void> => {
-    if (!file.url && !file.preview) {
-      file.preview = await getBase64(file.originFileObj!);
-    }
-    this.previewImage = file.url || file.preview;
-    this.previewVisible = true;
-  };
 
   handleChange() {
     this.router.navigateByUrl('app/photos')
@@ -44,6 +27,24 @@ export class AddPhotoComponent {
   handleRemove = (file: NzUploadFile) => {
     this.photoService.deletePhotoById(file.response.id).subscribe();
     return true
+  }
+
+  beforeUpload = (file: NzUploadFile): boolean => {
+    this.fileList = this.fileList.concat(file);
+
+    return false;
+  };
+
+  handleUpload(): void {
+    const observablesArray = []
+    for (const nzFile of this.fileList) {
+      const file = nzFile as any
+      delete file.thumbUrl
+      const formData = new FormData();
+      formData.append('file', file);
+      observablesArray.push(this.photoService.createOptimizedWebpPhoto({ formData: { file } }))
+    }
+    zip(observablesArray).pipe(tap(() => this.router.navigateByUrl('/app/photos'))).subscribe()
   }
 
 }
